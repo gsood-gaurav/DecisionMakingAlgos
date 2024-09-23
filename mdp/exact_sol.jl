2df0b8be25ceee586c347f40b09d6d079cf5062c
Gaurav Sood <gsood.gaurav@gmail.com>
2024-09-23 18:55:52 +0530

    Added MDPs

---
+++ mdp/exact_sol.jl
@@ -0,0 +1,11 @@
+# Exact Solution methods for Markov Decision Processes
+
+struct MDP
+    Y # distcount Factor
+    S # State Space
+    A # Action Space
+    T # Transistion function
+    R # Reward function
+    TR # Sample Transistion and Reward
+end
+
